85f8823cc34afb6d91169752df663413cc8616ab
RVHU321018UR <ns2617@nyu.edu>
2015-08-19 12:15:48 -0400

    database example

--- ServletExample/WebContent/output.jsp
+++ ServletExample/WebContent/output.jsp
@@ -3,7 +3,7 @@
 <!DOCTYPE html PUBLIC "-//W3C//DTD HTML 4.01 Transitional//EN" "http://www.w3.org/TR/html4/loose.dtd">
 <html>
 <head>
-<title>Shiny Bank: History</title>
+<title>Servlet</title>
 <meta charset="utf-8">
 <meta name="viewport" content="width=device-width, initial-scale=1">
 <link rel="stylesheet"
--- ServletExample/src/HelloWorld.java
+++ ServletExample/src/HelloWorld.java
@@ -2,6 +2,8 @@
 
 import java.io.IOException;
 import java.io.PrintWriter;
+import java.sql.*;
+import java.util.Properties;
 
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
@@ -30,15 +32,43 @@ public class HelloWorld extends HttpServlet {
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		//PrintWriter write =response.getWriter();
 		//write.println("<h1> Hello World </h1>");
-		request.setAttribute("message", "Hello World");
+		//request.setAttribute("message", "Hello World");
+		//getServletContext().getRequestDispatcher("/output.jsp").forward(request, response);
+		
+		
+		String url = "jdbc:oracle:thin:testuser/password@localhost"; 
+		String name="";
+	   	Properties props = new Properties();
+	   	props.setProperty("user", "testdb");
+        props.setProperty("password", "password");
+		try
+        {
+             Class.forName("oracle.jdbc.driver.OracleDriver");
+             Connection con=DriverManager.getConnection(url,props);
+             Statement st=con.createStatement();
+             System.out.println("connection established successfully...!!");     
+ 
+             ResultSet rs=st.executeQuery("Select * from Demo_Customers where CUSTOMER_ID=2");
+ 
+             rs.next();
+           	 name = rs.getString("CUST_FIRST_NAME") + " " + rs.getString("CUST_LAST_NAME");
+           	 System.out.println(name);
+             
+        }
+        catch (Exception e){
+            e.printStackTrace();
+        }
+		request.setAttribute("message", name);
 		getServletContext().getRequestDispatcher("/output.jsp").forward(request, response);
+				
 	}
 
 	/**
 	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
 	 */
 	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		// TODO Auto-generated method stub
+
+ 
 	}
 
 }
